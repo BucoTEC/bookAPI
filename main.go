f743c37fc1a9034d9405cb64f6c44d7786fc2d8a
Adnan Bucalovic <adnan.bucalovic13@gmail.com>
2022-02-13 12:32:36 +0100

    added check in check out

--- main.go
+++ main.go
@@ -57,6 +57,51 @@ func getBookById(id string) (*Book, error ){
 	}
 	return nil, errors.New("book not foun")
 }
+func checkoutBook(c *gin.Context) {
+	id, ok := c.GetQuery("id")
+
+	if !ok {
+		c.IndentedJSON(http.StatusBadRequest, gin.H{"message": "Missing id query parameter."})
+		return
+	}
+
+	book, err := getBookById(id)
+
+	if err != nil {
+		c.IndentedJSON(http.StatusNotFound, gin.H{"message": "Book not found."})
+		return
+	}
+
+	if book.Quantity <= 0 {
+		c.IndentedJSON(http.StatusBadRequest, gin.H{"message": "Book not available."})
+		return
+	}
+
+	book.Quantity -= 1
+	c.IndentedJSON(http.StatusOK, book)
+}
+func returnBook(c *gin.Context) {
+	id, ok := c.GetQuery("id")
+
+	if !ok {
+		c.IndentedJSON(http.StatusBadRequest, gin.H{"message": "Missing id query parameter."})
+		return
+	}
+
+	book, err := getBookById(id)
+
+	if err != nil {
+		c.IndentedJSON(http.StatusNotFound, gin.H{"message": "Book not found."})
+		return
+	}
+
+	book.Quantity += 1
+	c.IndentedJSON(http.StatusOK, book)
+}
+
+
+
+
 
 func main() {
 	router := gin.Default()
@@ -64,5 +109,8 @@ func main() {
 	router.GET("/books", getBooks )
 	router.GET("/books/:id", bookById )
 	router.POST("/books", createBook )
+	router.PATCH("/checkout", checkoutBook)
+	router.PUT("/checkin", returnBook)
+
 	router.Run("localhost:5000")
 }
